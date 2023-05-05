cd423634ee1d943d698aebed316b239fa7780acf
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2023-05-05 09:34:27 +0500

    Role model changed

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name'];
+
+    const ROLE_PENDING = 0;
+    const ROLE_PROCESSING = 1;
+    const ROLE_CLOSED = 2;
 }
