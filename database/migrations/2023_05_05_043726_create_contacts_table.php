d765cb85ddb0787264b32df7f151e5b0d31be330
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2023-05-05 09:39:44 +0500

    Contact model migration changed

--- database/migrations/2023_05_05_043726_create_contacts_table.php
+++ database/migrations/2023_05_05_043726_create_contacts_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->integer('phone');
+            $table->text('subject');
+            $table->foreignId('role_id')->constrained();
             $table->timestamps();
         });
     }
