77a88f17864205523dd2ecc1e1c8aa46dec80bb6
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2024-02-19 22:08:52 +0500

    is_admin migration chanaged

--- database/migrations/2024_02_19_170606_add_is_admin_to_users_table.php
+++ database/migrations/2024_02_19_170606_add_is_admin_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->boolean('is_admin')->default(false); // Добавляет поле is_admin со значением по умолчанию false
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('is_admin'); // Удаляет поле при откате миграции
         });
     }
 };
