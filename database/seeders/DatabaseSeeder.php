93dc5f6438c386febad2d569a71c91ad34a12665
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2024-02-19 16:18:48 +0500

    user mock

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::factory(100)->create();
         $this->call(RegionSeeder::class);
     }
 }
