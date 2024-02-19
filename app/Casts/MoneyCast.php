9980ddccbedf19f0dd8913d9151e091c09a50203
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2024-02-19 22:46:23 +0500

    money cast changed

--- app/Casts/MoneyCast.php
+++ app/Casts/MoneyCast.php
@@ -14,7 +14,7 @@ class MoneyCast implements CastsAttributes
      */
     public function get(Model $model, string $key, mixed $value, array $attributes): mixed
     {
-        return $value;
+        return round(floatval($value) / 100, precision: 2);
     }
 
     /**
@@ -24,6 +24,6 @@ class MoneyCast implements CastsAttributes
      */
     public function set(Model $model, string $key, mixed $value, array $attributes): mixed
     {
-        return $value;
+        return round(floatval($value) * 100);
     }
 }
