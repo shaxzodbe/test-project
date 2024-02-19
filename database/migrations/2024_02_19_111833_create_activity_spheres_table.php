f25addde8a9691d62b44f5715296bcfb5760fd5c
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2024-02-19 16:37:41 +0500

    activity model, migration, seeder changed

--- database/migrations/2024_02_19_111833_create_activity_spheres_table.php
+++ database/migrations/2024_02_19_111833_create_activity_spheres_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('activity_spheres', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
             $table->timestamps();
         });
     }
--- database/seeders/ActivitySphereSeeder.php
+++ database/seeders/ActivitySphereSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\ActivitySphere;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,31 @@ class ActivitySphereSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $activities = [
+          'Stock Market',
+          'Bonds',
+          'Real Estate',
+          'Mutual Funds',
+          'Exchange-Traded Funds (ETFs)',
+          'Commodities',
+          'Private Equity',
+          'Hedge Funds',
+          'Venture Capital',
+          'Cryptocurrencies',
+          'Foreign Exchange (Forex)',
+          'Collectibles and Art',
+          'Peer-to-Peer Lending',
+          'Startups',
+          'Sustainable and Responsible Investing',
+          'Derivatives (Options, Futures)',
+          'Precious Metals (Gold, Silver)',
+          'Fixed Income Securities',
+          'Insurance Products (Annuities)',
+          'Cash and Cash Equivalents',
+        ];
+
+        foreach ($activities as $activity) {
+            ActivitySphere::create(['title' => $activity]);
+        }
     }
 }
