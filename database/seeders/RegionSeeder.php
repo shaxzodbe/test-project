70c13a46746ba110ead5beab149cc979a60793e4
Shaxzodbek Sulaymanov <sulaymanovshaxzod@gmail.com>
2024-02-19 15:07:20 +0500

    regions changed

--- database/seeders/RegionSeeder.php
+++ database/seeders/RegionSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Region;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,25 @@ class RegionSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $regions = [
+          'Андижанская область',
+          'Бухарская область',
+          'Джизакская область',
+          'Кашкадарьинская область',
+          'Навоийская область',
+          'Наманганская область',
+          'Самаркандская область',
+          'Сурхандарьинская область',
+          'Сырдарьинская область',
+          'Ташкентская область',
+          'Ферганская область',
+          'Хорезмская область',
+          'Республика Каракалпакстан',
+          'Город Ташкент',
+        ];
+
+        foreach ($regions as $region) {
+            Region::create(['title' => $region]);
+        }
     }
 }
